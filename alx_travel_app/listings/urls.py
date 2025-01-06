7b4e790aebdb212a8b7ac439ba05c427e39289a6
Borngod <aboa.boni360@gmail.com>
2025-01-06 23:40:59 +0000

    new path

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name='alx_travel_app.listings'
 urlpatterns = [
     path('',views.home_view)
 ]
--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('listings/',include('listings.urls')),
+    path('listings/',include('alx_travel_app.listings.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
